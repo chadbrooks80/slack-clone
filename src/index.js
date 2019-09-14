bc1a0dcbc12214086033ec7195df5371ebfc037a
Chad Brooks <chadbrooks80@gmail.com>
2019-09-14 21:24:23 +0800

    user registration w/ firebase

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import 'semantic-ui-css/semantic.min.css';
+
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 
